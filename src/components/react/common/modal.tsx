94eeaf20c831aef15dffbe02f81302da00e89204
Mohamadreza Mosalli <mohamadresaaa@gmail.com>
2024-11-09 19:23:42 +0330

    create modal

---
+++ src/components/react/common/modal.tsx
@@ -0,0 +1,48 @@
+/** @jsxImportSource react */
+import { type ReactNode } from "react";
+
+interface Props {
+  close: () => void;
+  children: any;
+  title: string;
+}
+
+export default function Modal({ close, children, title }: Props) {
+  return (
+    <div className="relative z-10">
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 z-20 w-screen overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0">
+          <div className="relative bg-gray-50 transform overflow-hidden rounded-lg shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+              <h3 className="text-xl font-semibold text-gray-900">{title}</h3>
+              <button
+                type="button"
+                className="text-gray-400 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                onClick={close}
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">close</span>
+              </button>
+            </div>
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
